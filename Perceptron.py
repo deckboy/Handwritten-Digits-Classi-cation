a38d1f60e6a9c87984ad1088846e0c3e18efa391
kouruiPete <kourui.mun@gmail.com>
2019-09-09 16:26:48 -0500

    hw1 first commit

---
+++ Perceptron.py
@@ -0,0 +1,66 @@
+import numpy as np
+import sys
+
+"""This script implements a two-class perceptron model.
+"""
+
+class perceptron(object):
+	
+	def __init__(self, max_iter):
+		self.max_iter = max_iter
+
+	def fit(self, X, y):
+		"""Train perceptron model on data (X,y).
+
+		Args:
+			X: An array of shape [n_samples, n_features].
+			y: An array of shape [n_samples,]. Only contains 1 or -1.
+
+		Returns:
+			self: Returns an instance of self.
+		"""
+		### YOUR CODE HERE
+
+		### END YOUR CODE
+		
+		return self
+
+	def get_params(self):
+		"""Get parameters for this perceptron model.
+
+		Returns:
+			W: An array of shape [n_features,].
+		"""
+		if self.W is None:
+			print("Run fit first!")
+			sys.exit(-1)
+		return self.W
+
+	def predict(self, X):
+		"""Predict class labels for samples in X.
+
+		Args:
+			X: An array of shape [n_samples, n_features].
+
+		Returns:
+			preds: An array of shape [n_samples,]. Only contains 1 or -1.
+		"""
+		### YOUR CODE HERE
+
+		### END YOUR CODE
+
+	def score(self, X, y):
+		"""Returns the mean accuracy on the given test data and labels.
+
+		Args:
+			X: An array of shape [n_samples, n_features].
+			y: An array of shape [n_samples,]. Only contains 1 or -1.
+
+		Returns:
+			score: An float. Mean accuracy of self.predict(X) wrt. y.
+		"""
+		### YOUR CODE HERE
+
+		### END YOUR CODE
+
+
